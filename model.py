070a819585b6244ff0dad5b5d890a6b5cee17aaf
= <babylearnmaths@gmail.com>
2020-05-09 16:52:17 +0700

    fix position id

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torch.optim.lr_scheduler import LambdaLR
-from Event.CNN.utils import load_vocab
+from utils import load_vocab
 
 
 class Config(object):
